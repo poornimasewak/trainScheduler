69a241a8fb77ab7b1e10f7a144241d59ae9f8553
poornima sewak <poornima.mathur.20@gmail.com>
2016-12-09 23:31:41 -0600

    final update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,82 @@
-  $(document).ready(function(){
+  $(document).ready(function() {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAdCdgJG2nhfcY6Odse_oe4mO28PgfcgUI",
-    authDomain: "trainscheduler-41e65.firebaseapp.com",
-    databaseURL: "https://trainscheduler-41e65.firebaseio.com",
-    storageBucket: "trainscheduler-41e65.appspot.com",
-    messagingSenderId: "424228557052"
-  };
-  firebase.initializeApp(config);
+      // Initialize Firebase
+      var config = {
+          apiKey: "AIzaSyAdCdgJG2nhfcY6Odse_oe4mO28PgfcgUI",
+          authDomain: "trainscheduler-41e65.firebaseapp.com",
+          databaseURL: "https://trainscheduler-41e65.firebaseio.com",
+          storageBucket: "trainscheduler-41e65.appspot.com",
+          messagingSenderId: "424228557052"
+      };
+      firebase.initializeApp(config);
 
-  var database = firebase.database();
+      var database = firebase.database();
 
-  //variables to store values from user.
-  var trainName;
-  var destination;
-  var firstTrainTime;
-  var frequency;
-  // var nextTrain;
-  // var tMinutesTillTrain;
-  var trains = [];
+      //variables to store values from user.
+      var trainName = '';
+      var destination = '';
+      var firstTrainTime = '';
+      var frequency = '';
+      var trains = [];
 
-  // Grabbing input values entered by user when submit button is clicked.
-  $('.form-horizontal').on('submit', function(){
+      // Grabbing input values entered by user when submit button is clicked.
+      $('.form-horizontal').on('submit', function() {
 
-      trainName = $('#input-train-name').val().trim();
-      destination = $('#input-destination').val().trim();
-      firstTrainTime = $('#first-train-time').val().trim();
-      frequency = $('#input-frequency').val().trim();
-    // console.log(moment(firstTrainTime, "HH:mm"));
-      // First Time (pushed back 1 year to make sure it comes before current time)
-      var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
-      console.log(firstTimeConverted);
+          trainName = $('#input-train-name').val().trim();
+          destination = $('#input-destination').val().trim();
+          firstTrainTime = $("#first-train-time").val().trim();
+          frequency = $('#input-frequency').val().trim();
 
-      // Current Time
-      var currentTime = moment();
+          //creating object to store data from user
+          var trainData = {
+              trainName: trainName,
+              destination: destination,
+              firstTrainTime: firstTrainTime,
+              frequency: frequency,
+          }
 
-      console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+          //pushing data object to firebase
+          database.ref().push(trainData);
 
-      // Difference between the times
-      var diffTime = moment().diff(moment(firstTimeConverted), "years");
-      console.log("DIFFERENCE IN TIME: " + diffTime);
-
-      // Time apart (remainder)
-      var tRemainder = diffTime % frequency;
-      console.log(tRemainder);
-
-      // Minute Until Train
-      var tMinutesTillTrain = frequency - tRemainder;
-      console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-      // Next Train
-      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-      console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-
-  	 database.ref().push({
-			 trainName: trainName,
-			 destination: destination,
-			 firstTrainTime: firstTrainTime,
-			 frequency: frequency,
-       nextTrain: nextTrain,
-       tMinutesTillTrain: tMinutesTillTrain
-		});
-     
-     
-     // render();
-
-     
-  	 return false;
-  });
-
-
-
-  database.ref().on("child_added", function(childSnapshot){
-        console.log(childSnapshot.val());
-
-        // trains.push(snapshot.val());
- $('#train-info').append('<tr><td>'+childSnapshot.val().trainName+
-        '<td>'+childSnapshot.val().destination+'<td>'+childSnapshot.val().frequency+
-        '<td>'+childSnapshot.val().nextTrain+'<td><input type="submit" value="X" class="remove-train btn btn-danger btn-sm">');
-      // console.log(trains[i]+   i);
-
-// render();
-        
+          return false;
       });
 
-  // function render() {
 
-  //   for(var i = 0; i < trains.length; i++){
-  //     // console.log(trains.length);
-  //     $('#train-info').append('<tr><td>'+trains[i].trainName+
-  //       '<td>'+trains[i].destination+'<td>'+trains[i].frequency+
-  //       '<td>'+trains[i].nextTrain);
-  //     console.log(trains[i]+   i);
-  //   }
-  // }
-  
-});
+      //getting data back from firebase to display on web page
+      database.ref().on("child_added", function(childSnapshot) {
+
+          var fTrainTime = childSnapshot.val().firstTrainTime;
+
+
+          var tFrequency = childSnapshot.val().frequency;
+
+          // Current Time
+          // var currentTime = moment();
+          // console.log('current time : '+moment(currentTime).format("HH:mm"));
+
+          // Difference between the times
+          var diffTime = moment().diff(moment.unix(fTrainTime), "minutes");
+
+          // Time apart (remainder)
+          var tRemainder = diffTime % tFrequency;
+
+          // Minute Until Train
+          var tMinutesTillTrain = tFrequency - tRemainder;
+
+          // Next Train
+          var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+
+          $('#train-info').append('<tr><td>' + childSnapshot.val().trainName +
+              '<td>' + childSnapshot.val().destination + '<td>' + childSnapshot.val().frequency +
+              '<td>' + moment(nextTrain).format("HH:mm") + '<td>' + tMinutesTillTrain + '<td><input type="submit" value="X" class="remove-train btn btn-danger btn-sm">');
+
+      });
+
+      //removing train data from the web page once user click on remove button
+      $("body").on("click", ".remove-train", function() {
+          $(this).closest('tr').remove();
+          var getKey = $(this).parent().parent().attr('id');
+          database.ref().child(getKey).remove();
+      });
+  });
